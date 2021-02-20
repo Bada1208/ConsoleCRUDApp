359237d00dd20349bc383aeb51b1c086bffa7d0f
Bada1208 <sysoievbogdan@gmail.com>
2021-02-20 17:32:56 +0100

    add some fix to JavaIOSpecialtyRepositoryTxt class

--- src/main/java/com/sysoiev/consoleapp/repository/txt/JavaIOSpecialtyRepositoryTxt.java
+++ src/main/java/com/sysoiev/consoleapp/repository/txt/JavaIOSpecialtyRepositoryTxt.java
@@ -12,16 +12,20 @@ import java.util.List;
 
 public class JavaIOSpecialtyRepositoryTxt implements SpecialtiesRepository {
     private final String filePath = "src\\main\\resources\\txt\\specialties.txt";
+    private List<String> FROM_FILE_LIST;
 
-    @Override
-    public Specialty getById(Long id) {
-        List<String> fromFile = null;
+
+    {
         try {
-            fromFile = Files.readAllLines(Paths.get(filePath));
+            FROM_FILE_LIST = Files.readAllLines(Paths.get(filePath));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (String s : fromFile) {
+    }
+
+    @Override
+    public Specialty getById(Long id) {
+        for (String s : FROM_FILE_LIST) {
             if (s.substring(0, s.indexOf(" ")).equals(String.valueOf(id))) {
                 return new Specialty(id, s.substring(s.indexOf(' ')));
             }
@@ -31,12 +35,10 @@ public class JavaIOSpecialtyRepositoryTxt implements SpecialtiesRepository {
 
     @Override
     public void deleteById(Long id) {
-        List<String> fromFile = null;
         try {
-            fromFile = Files.readAllLines(Paths.get(filePath));
-            fromFile.removeIf(s -> (s.substring(0, s.indexOf(" "))).equals(String.valueOf(id)));
+            FROM_FILE_LIST.removeIf(s -> (s.substring(0, s.indexOf(" "))).equals(String.valueOf(id)));
             FileWriter fileWriter = new FileWriter(filePath);
-            for (String s : fromFile) {
+            for (String s : FROM_FILE_LIST) {
                 fileWriter.write(s + "\n");
             }
             fileWriter.close();
@@ -48,14 +50,13 @@ public class JavaIOSpecialtyRepositoryTxt implements SpecialtiesRepository {
     @Override
     public Specialty update(Specialty item) {
         try {
-            List<String> fromFile = Files.readAllLines(Paths.get(filePath));
-            for (int i = 0; i < fromFile.size(); i++) {
-                String line = fromFile.get(i).substring(0, fromFile.get(i).indexOf(' '));
+            for (int i = 0; i < FROM_FILE_LIST.size(); i++) {
+                String line = FROM_FILE_LIST.get(i).substring(0, FROM_FILE_LIST.get(i).indexOf(' '));
                 if (line.equals(String.valueOf(item.getId()))) {
-                    fromFile.set(i, item.toString());
+                    FROM_FILE_LIST.set(i, item.toString());
                 }
             }
-            Files.write(Paths.get(filePath), fromFile);
+            Files.write(Paths.get(filePath), FROM_FILE_LIST);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -74,15 +75,9 @@ public class JavaIOSpecialtyRepositoryTxt implements SpecialtiesRepository {
 
     @Override
     public List<Specialty> getAll() {
-        List<String> fromFile = null;
         List<Specialty> specialtyList = new ArrayList<>();
-        try {
-            fromFile = Files.readAllLines(Paths.get(filePath));
-            for (String s : fromFile) {
-                specialtyList.add(new Specialty(Long.parseLong(s.substring(0, s.indexOf(" "))), s.substring(s.indexOf(" "))));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (String s : FROM_FILE_LIST) {
+            specialtyList.add(new Specialty(Long.parseLong(s.substring(0, s.indexOf(" "))), s.substring(s.indexOf(" "))));
         }
         return specialtyList;
     }
